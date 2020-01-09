79640b0cc492e8cfa2c77d6619dea69f43596291
BassamAzizi <101010bassam@gmail.com>
2020-01-09 23:55:01 +0100

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# Search-Menu
-Created with CodeSandbox
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles/main.css";
+
+export default function App() {
+  return (
+    <div className="input-container">
+      <label>Search for Movie </label>
+      <input type="text"/>
+    </div>
+  );
+}
