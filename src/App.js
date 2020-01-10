d8b9474207a6b3cc518ad39b7c44c044fb407980
BassamAzizi <101010bassam@gmail.com>
2020-01-10 21:55:09 +0100

    create the movies & data components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { Component } from "react";
+import Movie from "./components/movie";
 import "./styles/main.css";
 
-export default function App() {
-  return (
-    <div className="input-container">
-      <label>Search for Movie </label>
-      <input type="text"/>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="input-container">
+          <label>Search for Movie </label>
+          <input type="text" />
+        </div>
+        <div className="movies-container">
+          <Movie />
+        </div>
+      </div>
+    );
+  }
 }
+
+export default App;
---
+++ src/components/movie.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Movie = () => {
+  return (
+    <div className="movie-container">
+      <div className="movie-image" />
+      <p className="movie-title">Dark knight</p>
+      <p className="movie-year" />
+    </div>
+  );
+};
+
+export default Movie;
